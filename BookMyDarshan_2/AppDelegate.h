140bb34b8e3cb917b2eac440449c24d950882eef
welcome <welcome@welcomes-mac.local>
2016-10-12 14:59:44 +0530

    Initial Commit

---
+++ BookMyDarshan_2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BookMyDarshan_2
+//
+//  Created by welcome on 10/12/16.
+//  Copyright © 2016 home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BookMyDarshan_2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BookMyDarshan_2
+//
+//  Created by welcome on 10/12/16.
+//  Copyright © 2016 home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
